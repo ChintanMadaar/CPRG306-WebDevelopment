f169a1e56ab2b320de33c83137eb6198f15ba7ee
ChintanMadaar <142057863+ChintanMadaar@users.noreply.github.com>
2024-05-20 02:07:48 -0600

    Add files via upload

---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+const StudentInfo = () => {
+  return (
+    <div>
+      <h2>Chintan Madaar</h2>
+
+      <p>
+        <Link href="https://github.com/ChintanMadaar/CPRG306-WebDevelopment.git">
+          Link to my Github
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
