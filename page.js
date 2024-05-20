711a8d1d73e0c08e61fa168eb5ecdaa8f1699ef2
ChintanMadaar <142057863+ChintanMadaar@users.noreply.github.com>
2024-05-20 02:05:36 -0600

    Add files via upload

---
+++ page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const RootPage = () => {
+  return (
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        <Link href="/week-2">week-2</Link>
+      </p>
+    </div>
+  );
+};
+
+export default RootPage;
