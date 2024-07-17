48d7a2c0da364e9d42b67109f381a23e43502ced
ChintanMadaar <142057863+ChintanMadaar@users.noreply.github.com>
2024-07-16 23:25:47 -0600

    Update firebase.js

--- app/week-8/_utils/firebase.js
+++ app/week-8/_utils/firebase.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCLW4dL-40er183NyDPwUFfFsZM3FOeutI",
+  authDomain: "cprg306-assignments-ed6cf.firebaseapp.com",
+  projectId: "cprg306-assignments-ed6cf",
+  storageBucket: "cprg306-assignments-ed6cf.appspot.com",
+  messagingSenderId: "939183180048",
+  appId: "1:939183180048:web:ac6b5c422a2fa6be27bd95",
+  measurementId: "G-G6D1RM4829",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
